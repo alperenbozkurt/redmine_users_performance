adeaa96733c1d23df1e89e1b1886f8617aaa775f
alperenbozkurt <bozkurttalperen@gmail.com>
2017-03-29 13:59:42 +0300

    init.rb Edited

--- init.rb
+++ init.rb
@@ -1,8 +1,9 @@
 Redmine::Plugin.register :users_performance do
-  name 'Users Performance plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  name 'Users Performance Plugin'
+  author 'Alperen Bozkurt'
+  description 'A redmine plugin that graphically shows the time entries of users'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/alperenbozkurt/redmine_users_performance'
+  author_url 'http://alperenbozkurt.net/'
+
 end
