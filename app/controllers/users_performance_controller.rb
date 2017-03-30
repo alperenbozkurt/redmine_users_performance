c64bd8d8345613a14921e9e756cddf86fe5e1836
alperenbozkurt <bozkurttalperen@gmail.com>
2017-03-30 19:20:04 +0300

    Working hours were collected.

--- app/controllers/users_performance_controller.rb
+++ app/controllers/users_performance_controller.rb
@@ -3,5 +3,10 @@ class UsersPerformanceController < ApplicationController
 
   def index
     @project = Project.find(params[:project_id])
+    @time_entries = TimeEntry.where(project_id: @project)
+    @grouped_time_entries = Hash.new
+    @time_entries.each do |e|
+      @grouped_time_entries.has_key?(e.user.name) ? @grouped_time_entries[e.user.name] = @grouped_time_entries[e.user.name] + e.hours : @grouped_time_entries[e.user.name] = e.hours
+    end
   end
 end
---
+++ app/models/users_performance.rb
@@ -0,0 +1,3 @@
+class UsersPerformance < ActiveRecord::Base
+  unloadable
+end
