4cedd0d65a2a64a6da4b161bf8e07efa3077d4cf
alperenbozkurt <bozkurttalperen@gmail.com>
2017-03-30 19:30:46 +0300

    Percent of working hours was added.

--- app/controllers/users_performance_controller.rb
+++ app/controllers/users_performance_controller.rb
@@ -5,8 +5,10 @@ class UsersPerformanceController < ApplicationController
     @project = Project.find(params[:project_id])
     @time_entries = TimeEntry.where(project_id: @project)
     @grouped_time_entries = Hash.new
+    @total_time = 0
     @time_entries.each do |e|
       @grouped_time_entries.has_key?(e.user.name) ? @grouped_time_entries[e.user.name] = @grouped_time_entries[e.user.name] + e.hours : @grouped_time_entries[e.user.name] = e.hours
+      @total_time = @total_time + e.hours
     end
   end
 end
