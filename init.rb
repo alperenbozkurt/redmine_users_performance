258100010c86062cb14f3263484cc842736f8034
alperenbozkurt <bozkurttalperen@gmail.com>
2017-04-11 17:49:12 +0300

    Fix #1

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_users_performance do
   name 'Users Performance Plugin'
   author 'Alperen Bozkurt'
   description 'A redmine plugin that graphically shows the time entries of users'
-  version '0.1.0'
+  version '0.1.1'
   url 'https://github.com/alperenbozkurt/redmine_users_performance'
   author_url 'http://alperenbozkurt.net/'
 
